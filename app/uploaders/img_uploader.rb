fe3ccb57843c9e0ae3a6dd608e5e15abee78fbc4
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-07-15 00:10:12 +0900

    second commit

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -24,7 +24,7 @@ class ImgUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  ActionController::Base.helpers.asset_path("fallback/" + [version_name, "IMG_3794.JPG"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
     # デフォルト画像
