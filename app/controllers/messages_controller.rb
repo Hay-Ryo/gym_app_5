dac84174634abfd0c739730cb05ac4b798b6513d
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-07-22 12:09:24 +0900

    second commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,8 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to("/rooms/#{current_user.id}")
     else
-      render("/rooms/#{current_user.id}")
+      render "/rooms/#{current_user.id}"
+    end
     # 投稿されたメッセージをチャット参加者に配信
     # ActionCable.server.broadcast 'room_channel',{message: @message.template}
   end
