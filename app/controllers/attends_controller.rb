41a22d20be179294149dbfbb0a39294021520961
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-07-29 20:06:55 +0900

    second commit

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -5,8 +5,8 @@ class AttendsController < ApplicationController
     @attends_recently = @user.attends.order(:created_at).last(5)
     @user_name = @user.user_name
     @attends_count = Attend.where(id: @attends).count    
-    @month = (Time.now.beginning_of_month..Time.now.end_of_day).to_s
-    @year = (Time.now.beginning_of_year..Time.now.end_of_year).to_s
+    @month = (Time.current.beginning_of_month..Time.current.end_of_day).to_s
+    @year = (Time.current.beginning_of_year..Time.current.end_of_year).to_s
     @month_counts = current_user.attends.where('created_at > ?', @month).count
     @year_counts = current_user.attends.where('created_at > ?', @year).count
   end
@@ -16,8 +16,8 @@ class AttendsController < ApplicationController
     @user_name = @user.user_name
     @attends = @user.attends.page(params[:page]).order(created_at: :desc)
     @attends_count = Attend.where(id: @attends).count
-    @month = (Time.now.beginning_of_month..Time.now.end_of_day).to_s
-    @year = (Time.now.beginning_of_year..Time.now.end_of_year).to_s
+    @month = (Time.current.beginning_of_month..Timecurrent.end_of_day).to_s
+    @year = (Time.current.beginning_of_year..Time.current.end_of_year).to_s
     @month_counts = current_user.attends.where('created_at > ?', @month).count
     @year_counts = current_user.attends.where('created_at > ?', @year).count
   end
@@ -26,11 +26,11 @@ class AttendsController < ApplicationController
     @user = current_user
     @user_name=@user.user_name
 # 今日の初めから終わり
-    @day = (Time.now.beginning_of_day..Time.now.end_of_day).to_s
+    @day = (Time.current.beginning_of_day..Time.current.end_of_day).to_s
 # 今月の初めから終わり
-    @month = (Time.now.beginning_of_month..Time.now.end_of_day).to_s
+    @month = (Time.current.beginning_of_month..Time.current.end_of_day).to_s
 # 今年の初めから終わり 
-    @year = (Time.now.beginning_of_year..Time.now.end_of_year).to_s
+    @year = (Time.current.beginning_of_year..Time.current.end_of_year).to_s
 # 今日の参加人数
     @day_counts= Attend.where('created_at > ?', @day).count
 # 今月の総回数
@@ -47,7 +47,7 @@ class AttendsController < ApplicationController
     @attend = Attend.new(
     user_id: @user,
     user_name: current_user.user_name,
-    start_time: Time.now
+    start_time: Time.current
     )
     if @attend.save
       redirect_to("/attends/#{@user}")
