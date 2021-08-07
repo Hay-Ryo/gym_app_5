e45d80775d7a640ab09ab70305ef08036cb4637e
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-08-07 18:04:47 +0900

    second commit

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -11,7 +11,7 @@ class Attend < ApplicationRecord
 
 	def check_business_hours
 		if Attend.where(created_at: non_business_hours)
-			errors.add(:non_business_hours, "は営業時間外です")
+			redirect_to root_path, flash: {msg: "営業時間外です"}
 		end
 	end 
 end
