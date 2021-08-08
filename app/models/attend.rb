6588a86ef9b4f2f00e69e7a1dca4a3ff8f5f5748
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-08-08 17:05:25 +0900

    second commit

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -3,16 +3,16 @@ class Attend < ApplicationRecord
 
 	paginates_per 5
 
-  validate :check_business_hours
+  # validate :check_business_hours
 
-	def non_business_hours 
-    Time.current.beginning_of_day..Time.current.beginning_of_day.since(20.hour)
-	end
+	# def non_business_hours 
+  #   Time.current.beginning_of_day..Time.current.beginning_of_day.since(20.hour)
+	# end
 
-	def check_business_hours
-		# Attend.where(created_at: Time.current.beginning_of_day..Time.current.beginning_of_day.since(20.hour))
-		num = Time.strftime('%H%M').to_i
-  [*0..1500, *2300..2359].include?(num)
-		flash[:msg]= "営業時間外です"
-	end 
+	# def check_business_hours
+	# 	# Attend.where(created_at: Time.current.beginning_of_day..Time.current.beginning_of_day.since(20.hour))
+	# 	num = Time.strftime('%H%M').to_i
+  # [*0..1500, *2300..2359].include?(num)
+	# 	flash[:msg]= "営業時間外です"
+	# end 
 end
