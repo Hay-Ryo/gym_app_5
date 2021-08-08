db411891b5d0ed23bba1aec6616faee5a3ab8d06
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-08-08 14:42:17 +0900

    second commit

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -10,7 +10,9 @@ class Attend < ApplicationRecord
 	end
 
 	def check_business_hours
-		Attend.where(created_at: Time.current.beginning_of_day..Time.parse("10 am").strftime("%r"))
+		# Attend.where(created_at: Time.current.beginning_of_day..Time.current.beginning_of_day.since(20.hour))
+		num = Time.strftime('%H%M').to_i
+  [*0..1500, *2300..2359].include?(num)
 		flash[:msg]= "営業時間外です"
 	end 
 end
