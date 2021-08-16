772c298e3292e460b83390888a903dd87db32fc3
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-08-16 12:04:18 +0900

    second commit

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,5 +2,8 @@ class Schedule < ApplicationRecord
   belongs_to :user
   validates :content, :title, presence: true, on: :create
 
+  validates :content, presence: true, on: :update
+  validates :title, presence: true, on: :update
+
   paginates_per 3
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  # タイトル欄は空白でも送信できることにした
   validates :content, presence: true
 
 end
