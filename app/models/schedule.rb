b51e4a75077a9330bc361e5504a3013828f0c91b
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-08-16 12:17:08 +0900

    second commit

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,9 @@
 class Schedule < ApplicationRecord
   belongs_to :user
-  validates :content, :title, presence: true, on: :create
+  # validates :content, :title, presence: true, on: :create
+
+  validates :content, presence: true, on: :create
+  validates :title, presence: true, on: :create
 
   validates :content, presence: true, on: :update
   validates :title, presence: true, on: :update
