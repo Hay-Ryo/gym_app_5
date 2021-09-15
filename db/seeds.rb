ce59d654841d9747a79a20ffbc8ced9840e38a4c
R-bros <hayashiryousuke@hayashi-no-MacBook-Air.local>
2021-09-15 13:19:15 +0900

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,10 +33,40 @@ User.create(
     :color => "#8a8a8a"
     },
     {
+    :user_name => "毛利元就", 
+    :email => "moori@motonari", 
+    :password => "motonari",
+    :color => "#8a8a8a"
+    },
+    {
+    :user_name => "前田利家", 
+    :email => "da@te", 
+    :password => "masamune",
+    :color => "#8a8a8a"
+    },
+    {
+    :user_name => "真田幸村", 
+    :email => "sanada@yukimura", 
+    :password => "yukimura",
+    :color => "#8a8a8a"
+    },
+    {
     :user_name => "武田信玄", 
     :email => "ta@ke", 
     :password => "shingen",
     :color => "#d93063"
+    },
+    {
+    :user_name => "上杉謙信", 
+    :email => "uesugi@kenshin", 
+    :password => "kenshin",
+    :color => "#8a8a8a"
+    },
+    {
+    :user_name => "直江兼続", 
+    :email => "naoe@kanetsugu", 
+    :password => "kanetsugu",
+    :color => "#8a8a8a"
     }
   ]
   )
@@ -44,7 +74,7 @@ User.create(
   Schedule.create(
   [
     {
-    :title => "織田", 
+    :title => "織田試合", 
     :user_id => "1", 
     :content => "メインイベントでタイトルマッチ",
     :start_time => "2021-09-02 03:46:00"
@@ -52,7 +82,7 @@ User.create(
       {
     :title => "休館", 
     :user_id => "2", 
-    :content => "器具メンテンスのため、この日はジム閉鎖します",
+    :content => "メンテンスのため、この日はジム閉鎖します",
     :start_time => "2021-09-03"
       },
       {
@@ -66,6 +96,36 @@ User.create(
     :user_id => "1", 
     :content => "東京ドームで試合です",
     :start_time => "2021-09-19"
+      },
+      {
+    :title => "試合", 
+    :user_id => "1", 
+    :content => "後楽園ホールで試合です",
+    :start_time => "2021-09-20"
+      },
+      {
+    :title => "試合", 
+    :user_id => "3", 
+    :content => "東京ドームで試合です",
+    :start_time => "2021-09-21"
+      },
+      {
+    :title => "会議", 
+    :user_id => "1", 
+    :content => "東京ドームで試合です",
+    :start_time => "2021-09-28"
+      },
+      {
+    :title => "会議", 
+    :user_id => "2", 
+    :content => "東京ドームで試合です",
+    :start_time => "2021-09-29"
+      },
+      {
+    :title => "(仮)試合", 
+    :user_id => "1", 
+    :content => "まだ決定ではないですが、徳川の試合です",
+    :start_time => "2021-09-07"
       },
       {
       :title => "ジム休み", 
@@ -99,24 +159,24 @@ User.create(
     :content => "頑張りましょう"
       },
       {
-    :user_id => "1",
+    :user_id => "6",
     :content => "おはようございます"
       },
       {
-    :user_id => "2",
+    :user_id => "7",
     :content => "天気がいいですね"
       },
       {
-    :user_id => "4",
+    :user_id => "8",
     :content => "スケジュール更新したので見てください"
       },
       {
-    :user_id => "3",
-    :content => "スケジュール更新したので見てください"
+    :user_id => "9",
+    :content => "ありがとうございます"
       },
       {
-    :user_id => "5",
-    :content => "スケジュール更新したので見てください"
+    :user_id => "10",
+    :content => "よろしくお願いします"
       }
   
   ]
